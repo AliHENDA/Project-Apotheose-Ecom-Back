df98b0577f4bee1f61713ae561a0b60a6a3367b5
Etudiant O'clock <alihenda4@gmail.com>
2023-05-04 14:07:27 +0200

    Adding Stock ok

--- src/Form/InventoryType.php
+++ src/Form/InventoryType.php
@@ -6,15 +6,17 @@ use App\Entity\Inventory;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class InventoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('size')
-            ->add('stock')
-            ->add('product')
+            ->add('size', TextType::class)
+            ->add('stock', IntegerType::class)
+           // ->add('product')
         ;
     }
 
